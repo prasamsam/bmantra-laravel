679f862093efeb0ada2fe5c51cf301a18922d2b5
Prashant <Prashant@MacBook.local>
2018-02-08 15:21:22 +0545

    databse setup

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
-$url = parse_url(getenv('DATABASE_URL'));
-$host = $url["host"] ?? null;
-$username = $url["user"] ?? null;
-$password = $url["pass"] ?? null;
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
 $database = substr($url["path"],1);
 
 
@@ -19,7 +19,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql_production'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -47,29 +47,17 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
-
-        'pgsql_production' => [
-            'driver' => 'pgsql',
             'host' => $host,
             'database' => $database,
             'username' => $username,
             'password' => $password,
-            'charset' => 'utf8',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'schema' => 'public',
+
         ],
+
+
 
         'pgsql' => [
             'driver' => 'pgsql',
